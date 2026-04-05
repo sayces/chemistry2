f01afb6b367fe0b51218efb1c33560ad376c3ad4
sasha <sayces@mail.ru>
2026-04-05 13:05:45 +0300

    i ve deleted tailwind

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    additionalData: `@use "styles/theme" as *;`,
+    includePaths: [path.join(__dirname, "src")],
+  },
 };
 
 export default nextConfig;
