d1f033d24475995ff952724c8322b19bfd1f8298
sasha <sayces@mail.ru>
2026-04-15 11:47:10 +0300

    Works w/ MainLayout

--- src/app/(main-routes)/layout.tsx
+++ src/app/(main-routes)/layout.tsx
@@ -2,14 +2,15 @@
 
 import Header from "@/shared/components/header/Header";
 import Columns from "@/shared/components/interactiveColumns/Columns";
+import styles from "./MainRoutesLayout.module.scss";
 
 const MainRoutesLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
+    <div className={styles.layoutWrapper}>
       <Header />
       <Columns />
       {children}
-    </>
+    </div>
   );
 };
 
